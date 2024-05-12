cb232f592634dc50078127546e0b79ee07d79155
Shyamnatesan <shyamnatesan21@gmail.com>
2024-05-12 14:51:55 +0530

    removed unnecessary code

--- src/main/java/org/shyamnatesan/Main.java
+++ src/main/java/org/shyamnatesan/Main.java
@@ -4,8 +4,7 @@ package org.shyamnatesan;
 public class Main {
     public static void main(String[] args) {
         IOCContainer ioc = new IOCContainer();
-        FirstClass firstClass = (FirstClass) ioc.getClassInstance(FirstClass.class);
-        firstClass.FirstDummy();
+        
 
     }
 }
